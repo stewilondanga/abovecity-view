56214a072ebe5868ee06932ba9dcd2e6a4eee241
Stewart ilondanga <stilondanga@gmail.com>
2019-02-10 21:49:59 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,44 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+function generateBuildings() {
+  var buildings = $('.buildings');
+
+  buildings.each(function(index) {
+    var self = $(this),
+        parentBlock = self.parent('.block'),
+        buildingsLength = (parentBlock.hasClass('double-wide')) ? 18 : 9;
+
+    for (var i = 0; i < buildingsLength; i++) {
+      var randomDepth = Math.floor(Math.random() * 10) + 5,
+          building = $('<div class="building" data-depth="' + randomDepth + '">');
+
+      self.append(building);
+    }
+  });
+}
+
+function extrudeBuildings() {
+  var building = $('.building');
+
+  building.each(function(index) {
+    var self = $(this),
+        depth = self.data('depth');
+
+    for (var i = 0; i < depth; i++) {
+      var extrusion = $('<span class="extrusion">');
+
+      extrusion.css({
+        'z-index': i * -1,
+        'transform': 'translate3d(0,0,' + i * 0.25 + 'vw)',
+        'z-index': i
+      });
+
+      self.append(extrusion);
+    }
+  });
+};
+
+generateBuildings();
+extrudeBuildings();
